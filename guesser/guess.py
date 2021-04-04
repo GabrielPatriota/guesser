8b8a3b6438b235a17a628bee10cba0e29463b72e
GabrielPatriota <71847552+GabrielPatriota@users.noreply.github.com>
2021-04-04 14:27:59 -0300

    Add files via upload

--- guesser/guess.py
+++ guesser/guess.py
@@ -1,4 +1,5 @@
 import generate
+guesses = 0
 while 1:
     guess = int(input())
 
@@ -7,14 +8,21 @@ while 1:
     if difference <= 10:
         if difference >= 0:
             print("Close!\n")
+            guesses += 1
         if difference >= -10:
             print("Close!\n")
+            guesses += 1
         if difference <= -11:
             print("Oh that's a bad guess\n")
+            guesses += 1
     if difference > 10:
         if difference <= 100:
             print("Oh that's a bad guess\n")
+            guesses += 1
+
+    
 
     if guess == generate.x:
         print("Wow you got it!\n")
+        print("Hmmm... " + str(guesses) + " guesses...")
         generate.x
